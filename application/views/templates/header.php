e8b89c896a4a043d466b8858ca5d96d297445ab5
shakiel17 <hisoka.aboy@gmail.com>
2024-08-05 11:36:04 +0800

    GUI Inventory

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,92 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
+<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <h3>General</h3>
+                <ul class="nav side-menu">
+                  <li><a href="<?=base_url();?>main"><i class="fa fa-home"></i> Home</a>                    
+                  </li>
+                  <li><a><i class="fa fa-television"></i> Attendance <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="<?=base_url();?>daily_monitoring">Attendance Monitoring</a></li>
+                      <!-- <li><a href="form_advanced.html">Manage Attendance</a></li> -->
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-edit"></i> Payroll <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="<?=base_url();?>manage_payroll">Manage Payroll</a></li>
+                      <li><a href="#" data-toggle="modal" data-target=".printPayrollSummary">Print Payroll Summary</a></li>                      
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-cogs"></i> Settings <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="<?=base_url();?>manage_deductions">Deductions</a></li>
+                      <li><a href="<?=base_url();?>manage_payroll_period">Payroll Period</a></li>                      
+                      <li><a href="<?=base_url();?>manage_advances">Advances</a></li>
+                    </ul>
+                  </li>                  
+                </ul>
+              </div>
 
-<head>
-    <meta charset="utf-8">
-    <title>Online Shopping | Paint Center </title>
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="Free HTML Templates" name="keywords">
-    <meta content="Free HTML Templates" name="description">
-
-    <!-- Favicon -->
-    <link href="<?=base_url();?>design/img/favicon.ico" rel="icon">
-
-    <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"> 
-
-    <!-- Font Awesome -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-
-    <!-- Libraries Stylesheet -->
-    <link href="<?=base_url();?>design/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-
-    <!-- Customized Bootstrap Stylesheet -->
-    <link href="<?=base_url();?>design/css/style.css" rel="stylesheet">
-    <link rel="icon" href="<?=base_url();?>design/img/pclogo.png" type="img/png">
-</head>
-
-<body>
-    <!-- Topbar Start -->
-    <div class="container-fluid">
-        <!-- <div class="row bg-secondary py-2 px-xl-5">
-            <div class="col-lg-6 d-none d-lg-block">
-                <div class="d-inline-flex align-items-center">
-                    <a class="text-dark" href="">FAQs</a>
-                    <span class="text-muted px-2">|</span>
-                    <a class="text-dark" href="">Help</a>
-                    <span class="text-muted px-2">|</span>
-                    <a class="text-dark" href="">Support</a>
-                </div>
             </div>
-            <div class="col-lg-6 text-center text-lg-right">
-                <div class="d-inline-flex align-items-center">
-                    <a class="text-dark px-2" href="">
-                        <i class="fab fa-facebook-f"></i>
-                    </a>
-                    <a class="text-dark px-2" href="">
-                        <i class="fab fa-twitter"></i>
-                    </a>
-                    <a class="text-dark px-2" href="">
-                        <i class="fab fa-linkedin-in"></i>
-                    </a>
-                    <a class="text-dark px-2" href="">
-                        <i class="fab fa-instagram"></i>
-                    </a>
-                    <a class="text-dark pl-2" href="">
-                        <i class="fab fa-youtube"></i>
-                    </a>
-                </div>
-            </div>
-        </div> -->
-        <div class="row align-items-center py-3 px-xl-5">
-            <div class="col-lg-3 d-none d-lg-block">
-                <a href="<?=base_url();?>" class="text-decoration-none">
-                <h2><span class="text-primary font-weight-bold border px-3 mr-1">Online Paint Center</span></h2>
-                    <!-- <img src="<?=base_url();?>design/img/pclogo.png" width="100"> -->
-                </a>
-            </div>
-            <div class="col-lg-6 col-6 text-left">
-                <form action="">
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Search for products">
-                        <div class="input-group-append">
-                            <span class="input-group-text bg-transparent text-primary">
-                                <i class="fa fa-search"></i>
-                            </span>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="col-lg-3 col-6 text-right">
-                <a href="" class="btn border">
-                    <i class="fas fa-shopping-cart text-primary"></i>
-                    <span class="badge">0</span>
-                </a>
-                <a href="" class="btn border">
-                    <i class="fas fa-user text-primary"></i>
-                    <span class="badge">Login</span>
-                </a>                
-            </div>
-        </div>
-    </div>
