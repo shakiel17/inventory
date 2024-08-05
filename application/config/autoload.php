2432fea9d86e96a263394b6e54608a60bde1c002
shakiel17 <hisoka.aboy@gmail.com>
2024-08-05 11:39:40 +0800

    Vendor

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('Sales_model');
+$autoload['model'] = array('Inventory_model');
